3e2074f1cef94cf104cb6104efa57b347aec9960
FelixChoi <felix.choi@udptechnology.com>
2020-04-18 19:33:36 +0900

    web: display - change display format for latest server

--- web/lib/main.dart
+++ web/lib/main.dart
@@ -31,89 +31,36 @@ class ServerStatusPage extends StatefulWidget {
   _ServerStatusPageState createState() => _ServerStatusPageState();
 }
 
-@JsonSerializable(explicitToJson: true)
-class StLoadAvg {
-  StLoadAvg();
-  factory StLoadAvg.fromJson(Map<String, dynamic> json) => _$StLoadAvgFromJson(json);
-  Map<String, dynamic> toJson() => _$StLoadAvgToJson(this);
-
-  int cnt;
-  List<double> avg;
-
-  String status() {
-    return '[$cnt] ${avg[0].toStringAsFixed(1)},${avg[1].toStringAsFixed(1)},${avg[2].toStringAsFixed(1)}';
-  }
-}
-
-@JsonSerializable(explicitToJson: true)
-class StDisk {
-  StDisk();
-  factory StDisk.fromJson(Map<String, dynamic> json) => _$StDiskFromJson(json);
-  Map<String, dynamic> toJson() => _$StDiskToJson(this);
-
-  int free;
-  int total;
-  int used;
-
-  String status() {
-    final totGB = total / 1024/1024/1024;
-    final usedGB = used / 1024/1024/1024;
-    return '${usedGB.toStringAsFixed(1)}G/${totGB.toStringAsFixed(1)}G';
-  }
-}
-
-@JsonSerializable(explicitToJson: true)
-class StMem {
-  StMem();
-  factory StMem.fromJson(Map<String, dynamic> json) => _$StMemFromJson(json);
-  Map<String, dynamic> toJson() => _$StMemToJson(this);
-
-  int total;
-  double percent;
-
-  String status() {
-    final totMB = (total / 1024/1024).round();
-    return '$percent%(${totMB}MB)';
-  }
-}
-
-@JsonSerializable(explicitToJson: true)
-class StSwap {
-  StSwap();
-  factory StSwap.fromJson(Map<String, dynamic> json) => _$StSwapFromJson(json);
-  Map<String, dynamic> toJson() => _$StSwapToJson(this);
-
-  int total;
-  double percent;
-
-  String status() {
-    final totMB = (total / 1024/1024).round();
-    return '$percent%(${totMB}MB)';
-  }
-}
-@JsonSerializable(explicitToJson: true)
-class StStatus {
-  StStatus();
-  factory StStatus.fromJson(Map<String, dynamic> json) => _$StStatusFromJson(json);
-  Map<String, dynamic> toJson() => _$StStatusToJson(this);
-
-  double cpu;
-  StLoadAvg load;
-  StDisk disk;
-  StMem mem;
-  StSwap swap;
-  Map<String, dynamic> apps;
-}
-
+@JsonSerializable()
 class StItem {
-  bool groupFlag;
+  StItem(this.name, this.alertFlag, this.v);
+  factory StItem.fromJson(Map<String, dynamic> json) => _$StItemFromJson(json);
+  Map<String, dynamic> toJson() => _$StItemToJson(this);
+
   String name;
-  String status;
   bool alertFlag;
+  String v;
+}
 
-  List<StItem> lst;
+@JsonSerializable(explicitToJson: true)
+class StGroup {
+  StGroup(this.name, this.items);
+  factory StGroup.fromJson(Map<String, dynamic> json) => _$StGroupFromJson(json);
+  Map<String, dynamic> toJson() => _$StGroupToJson(this);
 
-  StItem(this.groupFlag, this.name, this.status, this.alertFlag);
+  String name;
+  List<StItem> items;
+}
+
+@JsonSerializable(explicitToJson: true)
+class StServer {
+  StServer(this.name, this.items, this.groups);
+  factory StServer.fromJson(Map<String, dynamic> json) => _$StServerFromJson(json);
+  Map<String, dynamic> toJson() => _$StServerToJson(this);
+
+  String name;
+  List<StItem> items;
+  List<StGroup> groups;
 }
 
 String duration2str(Duration d) {
@@ -133,59 +80,8 @@ String duration2str(Duration d) {
   return ss;
 }
 
-@JsonSerializable(explicitToJson: true)
-class Server {
-  Server();
-  factory Server.fromJson(Map<String, dynamic> json) => _$ServerFromJson(json);
-  Map<String, dynamic> toJson() => _$ServerToJson(this);
-
-  String name;
-  StStatus status;
-
-  List<StItem> getStatus() {
-    final lst = List<StItem>();
-    if(status.cpu != null) {
-      lst.add(StItem(false, 'cpu', '${status.cpu}%', status.cpu > 80));
-    }
-    if(status.load != null) {
-      lst.add(StItem(false, 'load', '${status.load.status()}', false));
-    }
-    if(status.mem != null) {
-      lst.add(StItem(false, 'mem', '${status.mem.status()}', false));
-    }
-    if(status.swap != null) {
-      lst.add(StItem(false, 'swap', '${status.swap.status()}', status.swap.percent > 90));
-    }
-    if(status.disk != null) {
-      lst.add(StItem(false, 'disk', '${status.disk.status()}', status.disk.free < 1024*1024*1024*5));
-    }
-
-    if(status.apps != null) {
-      final apps = status.apps;
-      for(var appName in apps.keys) {
-        final appSt = StItem(true, appName, '', false);
-        lst.add(appSt);
-
-        final subList = List<StItem>();
-        appSt.lst = subList;
-        final app = apps[appName];
-        if(app.containsKey('err')) {
-          subList.add(StItem(false, 'err', app['err'], true));
-        } else {
-          final ts = apps[appName]['ts'];
-          final now = DateTime.now().millisecondsSinceEpoch/1000;
-          final d = Duration(seconds: (now-ts).round());
-          subList.add(StItem(false, 'alive', '${duration2str(d)}', d.inSeconds > 60));
-        }
-      }
-    }
-    return lst;
-  }
-
-}
-
 class _ServerStatusPageState extends State<ServerStatusPage> {
-  List<Server> servers = [];
+  List<StServer> servers = [];
   
   Future<void> _refresh() async {
     await doStatus();
@@ -209,13 +105,12 @@ class _ServerStatusPageState extends State<ServerStatusPage> {
     print('map - $map');
 
 
-    List<Server> newServers = [];
+    List<StServer> newServers = [];
     for(final item in map) {
-      newServers.add(Server.fromJson(item as Map<String, dynamic>));
+      newServers.add(StServer.fromJson(item));
     }
-    print('new ser - ${newServers[0].status.mem}');
     servers = newServers;
-    print('server - $servers');
+    //print('server - $servers');
   }
 
   void _initCode() async {
@@ -240,36 +135,39 @@ class _ServerStatusPageState extends State<ServerStatusPage> {
       body: ListView.builder(
         itemCount: servers.length,
         itemBuilder: (context, index) {
-          final lstSt = servers[index].getStatus();
+          final ser = servers[index];
           final lst = List<Widget>();
-          lst.add(Text('${servers[index].name} - '));
+          lst.add(Text('${ser.name} - '));
+
+          for(var item in ser.items) {
+            final txt = Text('${item.name}: ${item.v} ', 
+              textAlign: TextAlign.left,
+              style: TextStyle(color: item.alertFlag ? Colors.red : Colors.black));
+            lst.add(txt);
+          }
+
           final lstGroup = <Widget>[];
-          for(var item in lstSt) {
-            if(item.groupFlag) {
-              final children = <Widget>[];
-              children.add(Text('  ${item.name} -> ', textAlign: TextAlign.left));
+          //for(var i = 0; i < ser.groups.length; ++i) {
+          //  final group = ser.groups[i];
+          for(var group in ser.groups) {
+            final children = <Widget>[];
+            children.add(Text('  ${group.name} -> ', textAlign: TextAlign.left));
 
-
-              final lstRows = <Widget>[]; // 별도 행으로 표시할 아이템은 여기에
-              for(var subItem in item.lst) {
-                final txt = Text('${subItem.name}: ${subItem.status}', textAlign: TextAlign.left,
-                  style: TextStyle(color: subItem.alertFlag ? Colors.red : Colors.black));
+            final lstRows = <Widget>[]; // 별도 행으로 표시할 아이템은 여기에
+            print('item - ${group.items}');
+            if(group.items != null) {
+              for(var item in group.items) {
+                final txt = Text('${item.name}: ${item.v}', textAlign: TextAlign.left,
+                  style: TextStyle(color: item.alertFlag ? Colors.red : Colors.black));
                 children.add(txt);
               }
-              //lstRows.add(Text('haha'));
+            }
 
-              if(lstRows.length > 0) {
-                lstRows.insert(0, Row(children: children));
-                lstGroup.add(Column(children: lstRows));
-              } else {
-                lstGroup.add(Row(children: children));
-
-              }
+            if(lstRows.length > 0) {
+              lstRows.insert(0, Row(children: children));
+              lstGroup.add(Column(children: lstRows));
             } else {
-              final txt = Text('${item.name}: ${item.status} ', 
-                textAlign: TextAlign.left,
-                style: TextStyle(color: item.alertFlag ? Colors.red : Colors.black));
-              lst.add(txt);
+              lstGroup.add(Row(children: children));
             }
           }
 
--- web/lib/main.g.dart
+++ web/lib/main.g.dart
@@ -6,82 +6,46 @@ part of 'main.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-StLoadAvg _$StLoadAvgFromJson(Map<String, dynamic> json) {
-  return StLoadAvg()
-    ..cnt = json['cnt'] as int
-    ..avg = (json['avg'] as List)?.map((e) => (e as num)?.toDouble())?.toList();
+StItem _$StItemFromJson(Map<String, dynamic> json) {
+  return StItem(
+      json['name'] as String, json['alertFlag'] as bool, json['v'] as String);
 }
 
-Map<String, dynamic> _$StLoadAvgToJson(StLoadAvg instance) =>
-    <String, dynamic>{'cnt': instance.cnt, 'avg': instance.avg};
-
-StDisk _$StDiskFromJson(Map<String, dynamic> json) {
-  return StDisk()
-    ..free = json['free'] as int
-    ..total = json['total'] as int
-    ..used = json['used'] as int;
-}
-
-Map<String, dynamic> _$StDiskToJson(StDisk instance) => <String, dynamic>{
-      'free': instance.free,
-      'total': instance.total,
-      'used': instance.used
-    };
-
-StMem _$StMemFromJson(Map<String, dynamic> json) {
-  return StMem()
-    ..total = json['total'] as int
-    ..percent = (json['percent'] as num)?.toDouble();
-}
-
-Map<String, dynamic> _$StMemToJson(StMem instance) =>
-    <String, dynamic>{'total': instance.total, 'percent': instance.percent};
-
-StSwap _$StSwapFromJson(Map<String, dynamic> json) {
-  return StSwap()
-    ..total = json['total'] as int
-    ..percent = (json['percent'] as num)?.toDouble();
-}
-
-Map<String, dynamic> _$StSwapToJson(StSwap instance) =>
-    <String, dynamic>{'total': instance.total, 'percent': instance.percent};
-
-StStatus _$StStatusFromJson(Map<String, dynamic> json) {
-  return StStatus()
-    ..cpu = (json['cpu'] as num)?.toDouble()
-    ..load = json['load'] == null
-        ? null
-        : StLoadAvg.fromJson(json['load'] as Map<String, dynamic>)
-    ..disk = json['disk'] == null
-        ? null
-        : StDisk.fromJson(json['disk'] as Map<String, dynamic>)
-    ..mem = json['mem'] == null
-        ? null
-        : StMem.fromJson(json['mem'] as Map<String, dynamic>)
-    ..swap = json['swap'] == null
-        ? null
-        : StSwap.fromJson(json['swap'] as Map<String, dynamic>)
-    ..apps = json['apps'] as Map<String, dynamic>;
-}
-
-Map<String, dynamic> _$StStatusToJson(StStatus instance) => <String, dynamic>{
-      'cpu': instance.cpu,
-      'load': instance.load?.toJson(),
-      'disk': instance.disk?.toJson(),
-      'mem': instance.mem?.toJson(),
-      'swap': instance.swap?.toJson(),
-      'apps': instance.apps
-    };
-
-Server _$ServerFromJson(Map<String, dynamic> json) {
-  return Server()
-    ..name = json['name'] as String
-    ..status = json['status'] == null
-        ? null
-        : StStatus.fromJson(json['status'] as Map<String, dynamic>);
-}
-
-Map<String, dynamic> _$ServerToJson(Server instance) => <String, dynamic>{
+Map<String, dynamic> _$StItemToJson(StItem instance) => <String, dynamic>{
       'name': instance.name,
-      'status': instance.status?.toJson()
+      'alertFlag': instance.alertFlag,
+      'v': instance.v
+    };
+
+StGroup _$StGroupFromJson(Map<String, dynamic> json) {
+  return StGroup(
+      json['name'] as String,
+      (json['items'] as List)
+          ?.map((e) =>
+              e == null ? null : StItem.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$StGroupToJson(StGroup instance) => <String, dynamic>{
+      'name': instance.name,
+      'items': instance.items?.map((e) => e?.toJson())?.toList()
+    };
+
+StServer _$StServerFromJson(Map<String, dynamic> json) {
+  return StServer(
+      json['name'] as String,
+      (json['items'] as List)
+          ?.map((e) =>
+              e == null ? null : StItem.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      (json['groups'] as List)
+          ?.map((e) =>
+              e == null ? null : StGroup.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$StServerToJson(StServer instance) => <String, dynamic>{
+      'name': instance.name,
+      'items': instance.items?.map((e) => e?.toJson())?.toList(),
+      'groups': instance.groups?.map((e) => e?.toJson())?.toList()
     };
