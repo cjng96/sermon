97b5708f9635926871c209f29a03503c66addde7
alex.kim <alexgim961101@gmail.com>
2024-06-17 17:56:59 +0900

    Remove comment

--- server/helper.py
+++ server/helper.py
@@ -95,7 +95,6 @@ def main():
                                 ss = fp.read()
                             st = json.loads(ss)
                         except Exception as e:
-                            # TODO: 1. alertLevel = 2로 추가 [OK] -> alertFlag의 하위 호환성을 위해
                             st = dict(err=dict(v=str(e), alertFlag=True, alertLevel=2))
 
                         result["apps"][item["name"]] = st
@@ -104,7 +103,6 @@ def main():
                         raise Exception(f"unknown dict type[{tt}]")
 
                 else:
-                    # TODO: 2. alertLevel = 2로 추가 [OK] -> alertFlag의 하위 호환성을 위해
                     result["err"] = dict(v="unknown monitor data type[%s]!!" % type(item), alertFlag=True, alertLevel=2)
 
             log.write("result %s\n" % json.dumps(result))
